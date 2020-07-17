3a6f3fe598043c2f0a60dc57a5ad6a916102cc2c
Zerrtry <zerrtry@gmail.com>
2020-07-17 18:17:20 -0500

    logic for user management sys

--- models/review.js
+++ models/review.js
@@ -59,7 +59,7 @@ exports.checkReview = async (user_obj) => {
 exports.login = async (user_obj) => {
     console.log("Model login", user_obj);
     const connection = await mysql.connect();
-    const [data] = await connection.query(`SELECT * FROM user WHERE user_name = ? and email= ?`, [user_obj.user_name, user_obj.email]);
-    console.log("Query resonse", response.statusCode);
+    const data = await connection.query(`SELECT * FROM user WHERE user_name = ? and email= ?`, [user_obj.user_name, user_obj.email]);
+    console.log("Query resonse", data[0]);
     return data[0];
 }
